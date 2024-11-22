2cbdc40a1365f758b37bde33229ee8632f4357b2
rasulali <resul.eliyev2000@mail.ru>
2024-11-22 20:09:36 +0400

    some test

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
   return (
     <>
       <title>404: This page could not be found.</title>
-      <div className="h-svh flex items-center justify-center bg-black">
+      <div className="h-dvh flex items-center justify-center bg-black">
         <div className="flex flex-col gap-y-4 items-end">
           <h1 className="text-3xl text-white">404 | Not Found</h1>
           <Link href="/" className="text-center text-white/50 text-lg font-medium uppercase
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+"use client"
+import Logo from "./components/logo";
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="p-4 text-4xl font-bold">Rasul Alee</h1>
-    </div>
+    <section>
+      <div className="drop-shadow py-2 px-6 bg-white">
+        <Logo />
+      </div>
+    </section>
   );
 }

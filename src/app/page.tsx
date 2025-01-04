3fe206ae3a038aaae4d5f52b957ebb68c202e2fd
rasulali <resul.eliyev2000@mail.ru>
2025-01-04 06:43:26 +0400

    www domain

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 export default function Home() {
   return (
-    <main>
+    <main className="h-dvh flex items-center justify-center px-4">
+      <div className="mockup-code xl:w-1/2 text-xs xl:text-base">
+        <pre data-prefix="$"><code>launch personal-website</code></pre>
+        <pre data-prefix=">" className="text-success"><code>Initializing portfolio components...</code></pre>
+        <pre data-prefix=">" className="text-warning"><code>Building site assets...</code></pre>
+        <pre data-prefix=">" className="text-info"><code>Deploying to <span className="underline">alee.az</span> ⚡️</code></pre>
+      </div>
     </main>
   );
 }

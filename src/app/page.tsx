76aeb4cd7c526c6553e121969be68c7b2c795e1a
rasulali <resul.eliyev2000@mail.ru>
2025-03-28 16:51:18 +0400

    backup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
 'use client'
-import Divier from "../components/divier";
 import Nav from "../components/nav";
 
 export default function Home() {
   return (
     <main className="relative">
       <Nav />
-      <section className="p-6 min-h-screen">
-        <div>
-          <h1 className="text-5xl font-semibold">I deliver your digital footprint</h1>
-        </div>
-        <Divier text="about" />
+      <section className="p-6 min-h-screen relative">
       </section>
     </main>
   );
